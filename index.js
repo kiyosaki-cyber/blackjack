cfedc16910712381acd9bd08c07c12bdfa581f12
kiyosaki-cyber <134641062+kiyosaki-cyber@users.noreply.github.com>
2023-06-02 10:03:35 +0300

    created a new game button
    i have now created a new game function and made it to function on click

--- index.js
+++ index.js
@@ -1,38 +1,63 @@
-//declaring the vaariable sum
+//declaring the variable sum
 let firstCard  = 11
-let secondCard = 11
+let secondCard = 6
 let sum = firstCard + secondCard
 let sumEl = document.getElementById("sum-el")
 
 
 //declaring the cards variable
-let cards = "cards:" + firstCard + " , " + secondCard
+let cards = [firstCard, secondCard]
 let cardsEl = document.getElementById("cards-el")
 
-
 //displaying the message
+
+let message =" "
  let messageEl = document.getElementById("message-el")
 
- // setting the conditions of the game 
- if (sum === 21){
-    message = "You WON the blackjack"
-    
- }
- else if (sum > 21){
-    message = "You have lost the blackjack and OUT OF THE GAME"
- }
- else{
-    message = " Do you want to draw a NEW card"
-
- }
-
-
-//making the button funtion on click
-let buttonEl = document.getElementById("button-el")
 
 function startGame(){
-    buttonEl.textContent = "welcome to blackjack"
+    renderGame()
+ }
+function renderGame(){
+   
+ // setting the conditions of the game 
+ 
+ if (sum < 21)
+ { message = " Do you want to draw a NEW card ?"
+ }
+ else if (sum === 21){
+   message = "You WON the blackjack"
+}else{
+   message = "YOU'RE OUT OF THE GAME!"
+ }
+
     sumEl.textContent = "sum:" + sum 
-    cardsEl.textContent = cards
+    
+    cardsEl.textContent = "cards: "
+    for (let i = 0; i < cards.length; i++)
+    {
+      cardsEl.textContent += cards[i] + "  "
+    }
     messageEl.textContent = message
+    
 }
+function newCard(){ 
+    let card= 4
+    sum += card
+    cards.push(card)
+    renderGame()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
